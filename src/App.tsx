03d82d50fedf1ec6227383374b2a2d18dbd39791
adegale <156728691+adegale@users.noreply.github.com>
2026-05-30 22:54:02 -0400

    Add ExploreGrenada component to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Rooms from './components/Rooms';
 import Story from './components/Story';
+import ExploreGrenada from './components/ExploreGrenada';
 import Testimonials from './components/Testimonials';
 import Amenities from './components/Amenities';
 import Contact from './components/Contact';
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <Rooms />
       <Story />
+      <ExploreGrenada />
       <Testimonials />
       <Amenities />
       <Contact />
